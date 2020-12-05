d24a8177fceacbbf17190f9297fd44a1ac2d7c77
Dina <dmuhic@gmail.com>
2020-12-05 13:30:00 +0100

    fix layout issues, complete blur game

--- js/games.js
+++ js/games.js
@@ -1,72 +1,103 @@
 // blur game 
 
     // create objects to hold images along with movie titles
-    let img1 = {url: "../img/caligari.jpg", title:"the cabinet of dr caligari", year : "1920"};
-    let img2 = {url: "../img/cinematography.jpg", title: "vertigo"};
-    let img3 = {url: "../img/citizen.jpg", title: "citizen kane"};
-    let img4 = {url: "../img/editing.jpg", title: "alien"};
-    let img5 = {url: "../img/genre.jpg", title: "gentlemen prefer blondes"};
-    let img6 = {url: "../img/narrative.jpg", title: "memento"};
-    let img7 = {url: "../img/mise-en-scene.jpg", title: "the fifth element"};
+    let img1 = {url: "../img/caligari.jpg", title:"The Cabinet of Dr Caligari"};
+    let img2 = {url: "../img/cinematography.jpg", title: "Vertigo"};
+    let img3 = {url: "../img/citizen.jpg", title: "Citizen Kane"};
+    let img4 = {url: "../img/editing.jpg", title: "Alien"};
+    let img5 = {url: "../img/genre.jpg", title: "Gentlemen Prefer Blondes"};
+    let img6 = {url: "../img/narrative.jpg", title: "Memento"};
+    let img7 = {url: "../img/mise-en-scene.jpg", title: "The Fifth Element"};
     // store image objects in array
     const blurImgArray = [img1, img2, img3, img4, img5, img6, img7];
 
-    // pass random image from array to html img element
+    // prepare random image from array to pass to html img element
     const imgElement = document.querySelector("#blurImageContainer");
     const imgIndex = Math.floor((Math.random()*7));
-    imgElement.src = blurImgArray[imgIndex].url;
+    const image = blurImgArray[imgIndex];
+    
 
     // apply initial blur to image
-    let blurVal = 50;
+    let blurVal = 40;
     let blurAmount = "blur(" + blurVal + "px)";
-    imgElement.style.filter = blurAmount;
-
+    // set initial score and declare timer to be globally reusable
     let score = 100;
     let pauseTimer;
 
+    
+// game runs on button click (inline)
 function blurGame() {
-    // set up timer
-    pauseTimer = setInterval(blurTimer, 5000);
-
+    // url of image from array is passed to img element
+    imgElement.src = image.url;
+    // blur is applied
+    imgElement.style.filter = blurAmount;
+    // start button disappears, answer box appears
+    $("#startButton").hide();
+    $("fieldset").show();
+    // score displays on screen
+    $("#currentScore").text(score);
+    // times starts
+    pauseTimer = setInterval(blurTimer, 2000);
 }
    
-   function blurTimer() {
-       if (blurVal > 0) {
-       blurVal -= 5;
-       score -= 10;
-       blurAmount = "blur(" + blurVal + "px)";
-       imgElement.style.filter = blurAmount;
-       $("#currentScore").text(score);
-       } else {
-           clearInterval(pauseTimer);
-           alert("You ran out of time!");
+function blurTimer() {
+    // as long as the image is blurred, decrease score by 5 and blur by 3 every 3 seconds
+    if (blurVal > 0) {
+        blurVal -= 2;
+        score -= 5;
+        blurAmount = "blur(" + blurVal + "px)";
+        imgElement.style.filter = blurAmount;
+        // update score at every increment
+        $("#currentScore").text(score);
+    } else {
+        // once the blur is cleared, stop timer and inform user
+        clearInterval(pauseTimer);
+        alert("You ran out of time!");
        }
    }
     
-    // function runs on button click
+// function runs on guess button click
     function checkBlurGuess() {
+        // convert user input to lowercase
         const guess = $("#blurGuess:text").val().toLowerCase();
+        // pause timer
         clearInterval(pauseTimer);
+        // if input field is empty, continue timer
         if (guess === "") {
-            blurTimer();
+            pauseTimer = setInterval(blurTimer, 2000);
             return;
-        } else if (guess === blurImgArray[imgIndex].title) {
-            console.log("success");
-            $("#movieStill").text(blurImgArray[imgIndex].title);
+            // if guess matches image title
+        } else if (guess === image.title.toLowerCase()) {
+            // hide input field
+            $("fieldset").hide();
+            // set the success modal text to display correct title and score
+            $("#movieStill").text(image.title);
             $("#finalScore").text(score);
+            // desplay success modal
             $("#successModal").modal();
         } else {
-           $("failModal").modal();
-            
-            }
-            
-        
+            // if guess is incorrect, clear input field and display failure modal
+            $("#blurGuess:text").val("");
+            $("#failModal").modal();
+            } 
     } 
 
+    // event handler for OK button on success modal
     $("#ok-button").click(function() {
+        // remove blur from image, hide guess input, dismiss modal
         imgElement.style.filter = "blur(0px)";
         $("#successModal").modal("hide");
     })
 
-    // add event handlers to each button on modal
+   // event handler for Guess Again button on failure modal
+   $("#guess-again").click(function() {
+    // continue timer, dismiss modal
+    $("#failModal").modal("hide");
+    pauseTimer = setInterval(blurTimer, 2000);
+    })
+
+    $("#give-up").click(function() {
+        $("#failModal").modal("hide");
+        location.reload();
+        })
 
